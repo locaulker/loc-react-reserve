955d3fa1f30e8fffdff414231b26b33dea31ff78
Lawrence <Caulker>
2020-08-29 09:01:18 -0400

    addedd baseUrl process.env variable

--- pages/product.js
+++ pages/product.js
@@ -3,9 +3,10 @@ import axios from 'axios'
 
 import ProductSummary from '../components/Product/ProductSummary'
 import ProductAttributes from '../components/Product/ProductAttributes'
+import baseUrl from '../utils/baseUrl'
 
 Product.getInitialProps = async ({ query: { _id } }) => {
-  const url = 'http://localhost:3000/api/product'
+  const url = `${baseUrl}/api/product`
   const payload = { params: { _id } }
   const response = await axios.get(url, payload)
   return { product: response.data }
