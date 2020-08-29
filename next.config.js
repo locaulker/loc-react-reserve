f712371ac45af41f730148e6ee7e19b78b46edd8
Lawrence <Caulker>
2020-08-29 02:13:12 -0400

    modeled Product data and retrieved data from mogodb server

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   env: {
     MONGO_SRV:
-      'mongodb+srv://mainUser:sawa123xx@cluster0-x81cm.mongodb.net/<dbname>?retryWrites=true&w=majority',
+      'mongodb+srv://mainUser:sawa123xx@cluster0-x81cm.mongodb.net/loc-react-reserve?retryWrites=true&w=majority',
     JWT_SECRET: '<insert-jwt-secret>',
     CLOUDINARY_URL: '<insert-cloudinary-url>',
     STRIPE_SECRET_KEY: '<insert-stripe-secret-key>',
