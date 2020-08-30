d6547fa98019a2cae01781001f33743bb01f59de
Lawrence <Caulker>
2020-08-29 23:31:58 -0400

    created data collection input form

--- pages/create.js
+++ pages/create.js
@@ -1,7 +1,64 @@
 import React from 'react'
+import {
+  Form,
+  Input,
+  TextArea,
+  Button,
+  Image,
+  Message,
+  Header,
+  Icon,
+} from 'semantic-ui-react'
 
 function CreateProduct() {
-  return <>create</>
+  return (
+    <>
+      <Header as='h2' block>
+        <Icon name='add' color='orange' />
+        Create New Product
+      </Header>
+      <Form>
+        <Form.Group widths='equal'>
+          <Form.Field
+            control={Input}
+            name='name'
+            label='Name'
+            placeholder='Name'
+          />
+          <Form.Field
+            control={Input}
+            name='price'
+            label='Price'
+            placeholder='Price'
+            min='0.00'
+            step='0.01'
+            type='number'
+          />
+          <Form.Field
+            control={Input}
+            name='media'
+            type='file'
+            label='Media'
+            accept='image/*'
+            content='Select Image'
+          />
+        </Form.Group>
+        <Form.Field
+          control={TextArea}
+          name='description'
+          label='Description'
+          placeholder='Description'
+        />
+        <Form.Field
+          control={Button}
+          color='brown'
+          icon='pencil alternate'
+          content='Submit'
+          type='submit'
+        />
+      </Form>
+    </>
+  )
 }
 
 export default CreateProduct
