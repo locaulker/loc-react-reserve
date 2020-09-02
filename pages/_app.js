6d5f7e08fadebd3c5b8d396551fce4fe9fadf1b9
Lawrence <Caulker>
2020-09-02 14:56:42 -0400

    implemented invalid auth tokens

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import App from 'next/app'
 import Layout from '../components/_App/Layout'
-import { perseCookies, parseCookies } from 'nookies'
+import { parseCookies, destroyCookie } from 'nookies'
 import { redirectUser } from '../utils/auth'
 import baseUrl from '../utils/baseUrl'
 import axios from 'axios'
@@ -29,7 +29,11 @@ class MyApp extends App {
         const user = response.data
         pageProps.user = user
       } catch (error) {
-        console.error('Error getting user')
+        console.error('Error getting current user', error)
+        // 1) Throw out invalid token
+        destroyCookie(ctx, 'token')
+        //  2) Redirect to login page
+        redirectUser(ctx, '/login')
       }
     }
 
