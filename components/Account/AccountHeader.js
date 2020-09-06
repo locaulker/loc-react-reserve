3ce985bae68eb348cee3577933a36b5d51b08f28
Lawrence <Caulker>
2020-09-05 22:31:46 -0400

    added order history in accounts section

--- components/Account/AccountHeader.js
+++ components/Account/AccountHeader.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Header, Icon, Segment, Label } from 'semantic-ui-react'
 
-function AccountHeader() {
-  return <>AccountHeader</>
+function AccountHeader({ role, email, name, createdAt }) {
+  return (
+    <Segment secondary inverted color='violet'>
+      <Label
+        color='teal'
+        size='large'
+        ribbon
+        icon='privacy'
+        style={{ textTransform: 'capitalize' }}
+        content={role}
+      />
+      <Header inverted textAlign='center' as='h1' icon>
+        <Icon name='user' />
+        {name}
+        <Header.Subheader>{email}</Header.Subheader>
+        <Header.Subheader>Joined {createdAt}</Header.Subheader>
+      </Header>
+    </Segment>
+  )
 }
 
 export default AccountHeader
--- pages/account.js
+++ pages/account.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import AccountHeader from '../components/Account/AccountHeader'
+import AccountOrders from '../components/Account/AccountOrders'
+import { parseCookies } from 'nookies'
+import baseUrl from '../utils/baseUrl'
+import axios from 'axios'
 
-function Account() {
-  return <>account</>
+function Account({ user, orders }) {
+  console.log(orders)
+  return (
+    <>
+      <AccountHeader {...user} />
+      <AccountOrders orders={orders} />
+    </>
+  )
+}
+
+Account.getInitialProps = async ctx => {
+  const { token } = parseCookies(ctx)
+  if (!token) {
+    return { orders: [] }
+  }
+  const payload = { headers: { Authorization: token } }
+  const url = `${baseUrl}/api/orders`
+  const response = await axios.get(url, payload)
+  return response.data
 }
 
 export default Account
